27ec7d95d93e32293aa4552b51fb7b554c4cdac7
Piyush <pmish65@gmail.com>
2021-08-30 11:14:32 -0700

    Second Commit

--- RecursiveDigitSum/src/Solution.java
+++ RecursiveDigitSum/src/Solution.java
@@ -1,4 +1,27 @@
 
 public class Solution {
-	
+	public static int superDigit(String n, int k) {
+
+		long sum = 0;
+		long size = n.length();
+		for (int i = 0; i < size; i++) {
+			sum = sum + Integer.parseInt(String.valueOf(n.charAt(i)));
+
+		}
+		return helperFunction(String.valueOf(sum * k));
+
+	}
+
+	public static int helperFunction(String n) {
+		if (Long.parseLong(n) < 10) {
+			return Integer.parseInt(n);
+		}
+		long sum = 0;
+		long size = n.length();
+		for (int i = 0; i < size; i++) {
+			sum = sum + Integer.parseInt(String.valueOf(n.charAt(i)));
+
+		}
+		return helperFunction(String.valueOf(sum));
+	}
 }
