ef864f28e86432328f2d160352abd8a6a4fbd844
Piyush <pmish65@gmail.com>
2021-09-03 04:25:48 -0700

    SlidingWindow code

--- SlidingWindow/src/Solution.java
+++ SlidingWindow/src/Solution.java
@@ -1,4 +1,21 @@
+import java.util.List;
 
 public class Solution {
-
+	public static int slidingWindow(List<Integer> list,int k) {
+		int max=0,start=0,end=0,sum=0;
+		while(end<list.size()) {
+			sum+=list.get(end);
+			if((end-start+1)==k) {
+				if(max<sum) {
+					max=sum;
+				}
+				sum=sum-list.get(start);
+				start++;
+				end++;
+			}
+			end++;
+		}
+		return max;
+	}
+	
 }
