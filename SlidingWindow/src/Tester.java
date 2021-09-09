e1826ac0c2d13156278910ebfd7e08c5c95c3a73
Piyush <pmish65@gmail.com>
2021-09-09 02:05:09 -0700

    HackerRank Solution

---
+++ SlidingWindow/src/Tester.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Tester {
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(1);
+		list.add(3);
+		list.add(2);
+		System.out.println(Solution.slidingWindow(list, 3, 2));
+	}
+}
